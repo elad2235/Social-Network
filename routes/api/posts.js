960f811fb529e3e0617c1df701a03a328c8ace93
elad2235 <34191499+elad2235@users.noreply.github.com>
2020-08-12 18:05:54 +0300

    Started on client side HTML CSS and React

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,224 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const { post } = require("request");
+
+// @route   Post api/Posts
+// @desc    Create a post
+// access   Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById({ _id: req.user.id }).select(
+        "-password"
+      );
+      const newPost = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      const pst = new Post(newPost);
+      const post = await pst.save();
+      res.json(post);
+    } catch (e) {
+      console.error(e);
+      return res.status(500).json({ msg: "Server Error" });
+    }
+  }
+);
 
 // @route   GET api/Posts
-// @desc    Test Route
-// access   Public
-router.get('/', (req,res) => {
-    res.send('Posts Route');
+// @desc    Get all posts
+// access   Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ msg: "Server Error" });
+  }
 });
 
+// @route   GET api/Posts/:id
+// @desc    Get post by id
+// access   Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (e) {
+    if (e.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    console.error(e);
+    return res.status(500).json({ msg: "Server Error" });
+  }
+});
 
+// @route   DELETE api/Posts/:id
+// @desc    Delete post by id
+// access   Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    // Check Owner of post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await post.remove();
+    res.json({ msg: "Post removed" });
+  } catch (e) {
+    if (e.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    console.error(e);
+    return res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+// @route   PUT api/Posts/like/:id
+// @desc    Like a post
+// access   Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    if (
+      post.like.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+    post.like.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.like);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+// @route   PUT api/Posts/unlike/:id
+// @desc    Like a post
+// access   Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    if (
+      post.like.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+
+    // Get Remove index
+    removeIndex = post.like
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+    post.like.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.like);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+// @route   POST api/Posts/:comment/:id
+// @desc    Comment on a post
+// access   Private
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+
+      if (!post) {
+        return res.status(404).json({ msg: "Post not found." });
+      }
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+      post.save();
+      res.json(post.comments);
+    } catch (e) {
+      console.error(e);
+      return res.status(500).json({ msg: "Server Error" });
+    }
+  }
+);
+
+// @route   DELETE api/Posts/:comment/:id/:comment_id
+// @desc    Delete a comment from post
+// access   Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  const errors = validationResult(req);
+  try {
+    const post = await Post.findById(req.params.id);
+    // Get Comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment not found." });
+    }
+
+    // Check comment owner
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized." });
+    }
+    // Get Remove index
+    removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ msg: "Server Error" });
+  }
+});
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
 
 // @route   GET api/profile/me
 // @desc    Get Current user profile
@@ -201,7 +203,6 @@ router.put(
 // @route   DELETE api/profile/experience/:exp_id
 // @desc    Delete profile experience
 // @access  Private
-
 router.delete("/experience/:exp_id", auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.user.id });
@@ -214,6 +215,103 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
     res.json(profile);
   } catch (e) {
     console.error(e);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+// @route   PUT api/profile/Education
+// @desc    Add profile Education
+// @access  Private
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is required").not().isEmpty(),
+      check("degree", "Degree is required").not().isEmpty(),
+      check("from", "from date is required").not().isEmpty(),
+      check("fieldofstudy", "Field of study is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) return res.status(400).json({ errors: errors.array });
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json({ profile });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ msg: "Server Error" });
+    }
+  }
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete profile education
+// @access  Private
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // Get Remove index
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+// @route   GET api/profile/github/:username
+// @desc    get user repos from Github
+// @access  Public
+router.get("/github/:username", async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, async (error, response, body) => {
+      if (error) console.error(error);
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No Github profile found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (e) {
+    console.error(e.message);
     res.status(500).json({ msg: "Server Error" });
   }
 });
